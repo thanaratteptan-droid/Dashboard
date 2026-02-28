1f3b6f509e52e061d18fed11efc2b48179be9eaf
Thanarat Teptan <thanaratteptan@gmail.com>
2026-03-01 03:05:51 +0700

    page setting

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+# 1. ตั้งค่าหน้าเพจ
+st.set_page_config(page_title="2D Game Dashboard", layout="wide")
+st.title("🎮 2D Shooter Game Analytics")
+st.markdown("แดชบอร์ดสรุปสถิติผู้เล่นเกมชูตติ้ง 2 มิติ")
